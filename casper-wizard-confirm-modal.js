8770c52ac7fcf831af31bb0668f11ec077340d85
João Ramos <poor666@gmail.com>
2021-08-02 10:27:41 +0100

    allow confirm with reason

--- casper-wizard-confirm-modal.js
+++ casper-wizard-confirm-modal.js
@@ -21,6 +21,7 @@
 import { CasperWizard } from './casper-wizard.js';
 import { html } from '@polymer/polymer/lib/utils/html-tag.js';
 import { Casper } from '@cloudware-casper/casper-common-ui/casper-i18n-behavior.js';
+import { afterNextRender } from '@polymer/polymer/lib/utils/render-status.js';
 
 class ConfirmWizardConfirmModal extends Casper.I18n(CasperWizard) {
   static get template () {
@@ -57,6 +58,14 @@ class ConfirmWizardConfirmModal extends Casper.I18n(CasperWizard) {
 
       <casper-wizard-page id="confirm" hide-title next$="[[accept]]" previous$="[[reject]]">
         <div class="content"></div>
+        <template is="dom-if" if="[[needsReason]]" restamp>
+
+          <paper-input id="reason"
+            label="Motivo de anulação"
+            value="{{reason}}"
+            required
+          </paper-input>
+        </template>
       </casper-wizard-page>
     `;
   }
@@ -84,13 +93,22 @@ class ConfirmWizardConfirmModal extends Casper.I18n(CasperWizard) {
       needsPassword: {
         type: Boolean,
         value: false
+      },
+      needsReason: {
+        type: Boolean,
+        value: false
+      },
+      reason: {
+        type: String,
+        value: undefined
       }
     };
   }
 
   ready () {
     super.ready();
-    this.addEventListener('accept-modal', () => this._resolve && this._resolve({ ok: true }) );
+
+    this.addEventListener('accept-modal', () => this._resolve && this._resolve({ ok: true, reason: this.reason }) );
     this.addEventListener('reject-modal', () => this._reject && this._reject() );
     this.defaultOptions = {
       message: 'Tem a certeza que pretende prosseguir?',
@@ -122,6 +140,12 @@ class ConfirmWizardConfirmModal extends Casper.I18n(CasperWizard) {
 
   setOptions (options) {
 
+    if (options.needs_reason) {
+      this.defaultOverrideWizardDimensions = { width: '450px', height: '230px' };
+    } else {
+      this.defaultOverrideWizardDimensions = { width: '350px', height: '200px' };
+    }
+
     const wizardDimensions = {...this.defaultOverrideWizardDimensions, ...options.overrideWizardDimensions};
     // Changed functionality to merge button options, not replace
     const wizardButtons = options.overrideWizardButtons != undefined ? {
@@ -147,10 +171,21 @@ class ConfirmWizardConfirmModal extends Casper.I18n(CasperWizard) {
     super.connectedCallback();
     const content = this.shadowRoot.querySelector('.content');
     content.innerHTML = this.options.message;
+
+    this._resetReason();
+
+
     this.$.confirm.setAttribute("next", this.options.accept);
     this.$.confirm.setAttribute("previous", this.options.reject);
     this.appendPagesAndActivate(0);
     this.enablePrevious();
+
+    afterNextRender(this, () => {
+      if (this.shadowRoot.querySelector('paper-input')){
+        this.shadowRoot.querySelector('paper-input').focus()
+        this.shadowRoot.querySelector('paper-input').addEventListener('keydown', e => this._onKeyDown(e))
+      }
+    });
   }
 
   open () {
@@ -173,12 +208,39 @@ class ConfirmWizardConfirmModal extends Casper.I18n(CasperWizard) {
   }
 
   _gotoNextPage () {
-    this.dispatchEvent(new CustomEvent('accept-modal', { bubbles: true, composed: true }));
-    this.close();
+    let _canProceed = true
+
+    if (this.options?.needs_reason) {
+      _canProceed = this.shadowRoot.querySelector('paper-input').validate()
+    }
+
+    if (_canProceed) {
+      this.dispatchEvent(new CustomEvent('accept-modal', { bubbles: true, composed: true }));
+      this.close();
+    }
   }
 
   isValid () {
    return true;
+  }
+
+  _resetReason() {
+    if (this.options.needs_reason) {
+      this.needsReason = true
+    } else {
+      this.needsReason = false
+    }
+
+    this.reason = undefined
+    if (this.shadowRoot.querySelector('paper-input')){
+      this.shadowRoot.querySelector('paper-input').invalid = false
+    }
+  }
+
+  _onKeyDown (event) {
+    if ( event.keyCode == 13 ) {
+      this._gotoNextPage();
+    }
   }
 }
 
