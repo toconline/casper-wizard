e7873842b08c5f5fd114994ebafdfd698b54ed31
João Ramos <poor666@gmail.com>
2021-08-23 08:19:26 +0100

    FIX: missing close tag on paper-input; code formatted too

--- casper-wizard-confirm-modal.js
+++ casper-wizard-confirm-modal.js
@@ -24,224 +24,252 @@ import { Casper } from '@cloudware-casper/casper-common-ui/casper-i18n-behavior.
 import { afterNextRender } from '@polymer/polymer/lib/utils/render-status.js';
 
 class ConfirmWizardConfirmModal extends Casper.I18n(CasperWizard) {
-  static get template () {
-    return html`
-      <style>
-        .container {
-          display: flex;
-        }
+	static get template() {
+		return html`
+			<style>
+				.container {
+					display: flex;
+				}
 
-        .all_width {
-          width: 100%;
-        }
+				.all_width {
+					width: 100%;
+				}
 
-        casper-wizard-page .pagetitle {
-          display: none;
-        }
+				casper-wizard-page .pagetitle {
+					display: none;
+				}
 
-        .content {
-          font-size: 18px;
-        }
+				.content {
+					font-size: 18px;
+				}
 
-        .errors_list {
-          padding: 0 30px;
-          color: var(--dark-theme-background-color);
-        }
+				.errors_list {
+					padding: 0 30px;
+					color: var(--dark-theme-background-color);
+				}
 
-        .errors_list > li {
-          padding: 10px;
-          border-bottom: 1px var(--dark-theme-background-color) solid;
-          margin-bottom: 4px;
-        }
+				.errors_list > li {
+					padding: 10px;
+					border-bottom: 1px var(--dark-theme-background-color) solid;
+					margin-bottom: 4px;
+				}
+			</style>
 
-      </style>
+			<casper-wizard-page id="confirm" hide-title next$="[[accept]]" previous$="[[reject]]">
+				<div class="content"></div>
+				<template is="dom-if" if="[[needsReason]]" restamp>
+					<paper-input
+						id="reason"
+						label="Motivo de anulação"
+						value="{{reason}}"
+						required
+					/>
+				</template>
+			</casper-wizard-page>
+		`;
+	}
 
-      <casper-wizard-page id="confirm" hide-title next$="[[accept]]" previous$="[[reject]]">
-        <div class="content"></div>
-        <template is="dom-if" if="[[needsReason]]" restamp>
+	static get is() {
+		return 'casper-wizard-confirm-modal';
+	}
 
-          <paper-input id="reason"
-            label="Motivo de anulação"
-            value="{{reason}}"
-            required
-          </paper-input>
-        </template>
-      </casper-wizard-page>
-    `;
-  }
+	static get properties() {
+		return {
+			options: {
+				type: Object,
+				value: function () {
+					return {};
+				},
+			},
+			icon: {
+				type: String,
+				value: 'exclamation',
+			},
+			accountantPassword: {
+				notify: true,
+				type: String,
+			},
+			needsPassword: {
+				type: Boolean,
+				value: false,
+			},
+			needsReason: {
+				type: Boolean,
+				value: false,
+			},
+			reason: {
+				type: String,
+				value: undefined,
+			},
+		};
+	}
 
-  static get is () {
-    return 'casper-wizard-confirm-modal';
-  }
+	ready() {
+		super.ready();
 
-  static get properties () {
-    return {
-      options: {
-        type: Object,
-        value: function() {
-          return {};
-        }
-      },
-      icon: {
-        type: String,
-        value: 'exclamation'
-      },
-      accountantPassword: {
-        notify: true,
-        type: String
-      },
-      needsPassword: {
-        type: Boolean,
-        value: false
-      },
-      needsReason: {
-        type: Boolean,
-        value: false
-      },
-      reason: {
-        type: String,
-        value: undefined
-      }
-    };
-  }
+		this.addEventListener(
+			'accept-modal',
+			() => this._resolve && this._resolve({ ok: true, reason: this.reason })
+		);
+		this.addEventListener('reject-modal', () => this._reject && this._reject());
+		this.defaultOptions = {
+			message: 'Tem a certeza que pretende prosseguir?',
+			accept: 'sim',
+			reject: 'não',
+		};
 
-  ready () {
-    super.ready();
+		this.defaultOverrideWizardDimensions = {
+			width: '350px',
+			height: '200px',
+		};
 
-    this.addEventListener('accept-modal', () => this._resolve && this._resolve({ ok: true, reason: this.reason }) );
-    this.addEventListener('reject-modal', () => this._reject && this._reject() );
-    this.defaultOptions = {
-      message: 'Tem a certeza que pretende prosseguir?',
-      accept: 'sim',
-      reject: 'não'
-    };
+		this.defaultOverrideWizardButtons = {
+			previous: {
+				backgroundColor: '#E5E5E5',
+				color: '#A9A9A9',
+				display: 'flex',
+				border: 'none',
+			},
+			next: {
+				backgroundColor: '#EF5350',
+				display: 'flex',
+			},
+		};
 
-    this.defaultOverrideWizardDimensions = {
-      width: '350px',
-      height: '200px'
-    };
+		this.setOptions({});
+	}
 
-    this.defaultOverrideWizardButtons = {
-      previous: {
-        backgroundColor: '#E5E5E5',
-        color: '#A9A9A9',
-        display: 'flex',
-        border: 'none'
-      },
-      next: {
-        backgroundColor: '#EF5350',
-        display: 'flex'
-      }
-    };
+	setOptions(options) {
+		if (options.needs_reason) {
+			this.defaultOverrideWizardDimensions = {
+				width: '450px',
+				height: '230px',
+			};
+		} else {
+			this.defaultOverrideWizardDimensions = {
+				width: '350px',
+				height: '200px',
+			};
+		}
 
-    this.setOptions({});
+		const wizardDimensions = {
+			...this.defaultOverrideWizardDimensions,
+			...options.overrideWizardDimensions,
+		};
+		// Changed functionality to merge button options, not replace
+		const wizardButtons =
+			options.overrideWizardButtons != undefined
+				? {
+						previous: {
+							...this.defaultOverrideWizardButtons.previous,
+							...options.overrideWizardButtons.previous,
+						},
+						next: {
+							...this.defaultOverrideWizardButtons.next,
+							...options.overrideWizardButtons.next,
+						},
+				  }
+				: this.defaultOverrideWizardButtons;
 
-  }
+		this._closeButton.style.visibility = 'hidden';
 
-  setOptions (options) {
+		this.overrideWizardDimensions(wizardDimensions);
+		if (
+			options &&
+			options.hideReject &&
+			wizardButtons &&
+			wizardButtons.previous
+		) {
+			wizardButtons.previous.display = 'none';
+		}
+		this.overrideWizardButtons(wizardButtons);
 
-    if (options.needs_reason) {
-      this.defaultOverrideWizardDimensions = { width: '450px', height: '230px' };
-    } else {
-      this.defaultOverrideWizardDimensions = { width: '350px', height: '200px' };
-    }
+		super.setOptions({ ...this.defaultOptions, ...options });
+		if (options.title) {
+			super.setTitle(options.title);
+		}
+	}
 
-    const wizardDimensions = {...this.defaultOverrideWizardDimensions, ...options.overrideWizardDimensions};
-    // Changed functionality to merge button options, not replace
-    const wizardButtons = options.overrideWizardButtons != undefined ? {
-      previous: {...this.defaultOverrideWizardButtons.previous, ...options.overrideWizardButtons.previous},
-      next: {...this.defaultOverrideWizardButtons.next, ...options.overrideWizardButtons.next},
-    } : this.defaultOverrideWizardButtons;
+	connectedCallback() {
+		super.connectedCallback();
+		const content = this.shadowRoot.querySelector('.content');
+		content.innerHTML = this.options.message;
 
-    this._closeButton.style.visibility = 'hidden';
+		this._resetReason();
 
-    this.overrideWizardDimensions(wizardDimensions);
-    if ( options && options.hideReject && wizardButtons && wizardButtons.previous ) {
-      wizardButtons.previous.display = 'none';
-    }
-    this.overrideWizardButtons(wizardButtons);
+		this.$.confirm.setAttribute('next', this.options.accept);
+		this.$.confirm.setAttribute('previous', this.options.reject);
+		this.appendPagesAndActivate(0);
+		this.enablePrevious();
 
-    super.setOptions({...this.defaultOptions, ...options});
-    if ( options.title ) {
-      super.setTitle(options.title);
-    }
-  }
+		afterNextRender(this, () => {
+			if (this.shadowRoot.querySelector('paper-input')) {
+				this.shadowRoot.querySelector('paper-input').focus();
+				this.shadowRoot
+					.querySelector('paper-input')
+					.addEventListener('keydown', (e) => this._onKeyDown(e));
+			}
+		});
+	}
 
-  connectedCallback () {
-    super.connectedCallback();
-    const content = this.shadowRoot.querySelector('.content');
-    content.innerHTML = this.options.message;
+	open() {
+		super.open();
+	}
 
-    this._resetReason();
+	connection() {
+		return new Promise(
+			function (resolve, reject) {
+				this.open();
+				this._resolve = resolve;
+				this._reject = reject;
+			}.bind(this)
+		);
+	}
 
+	_gotoPreviousPage() {
+		this.dispatchEvent(
+			new CustomEvent('reject-modal', { bubbles: true, composed: true })
+		);
+		this.close();
+	}
 
-    this.$.confirm.setAttribute("next", this.options.accept);
-    this.$.confirm.setAttribute("previous", this.options.reject);
-    this.appendPagesAndActivate(0);
-    this.enablePrevious();
+	_gotoNextPage() {
+		let _canProceed = true;
 
-    afterNextRender(this, () => {
-      if (this.shadowRoot.querySelector('paper-input')){
-        this.shadowRoot.querySelector('paper-input').focus()
-        this.shadowRoot.querySelector('paper-input').addEventListener('keydown', e => this._onKeyDown(e))
-      }
-    });
-  }
+		if (this.options?.needs_reason) {
+			_canProceed = this.shadowRoot.querySelector('paper-input').validate();
+		}
 
-  open () {
-    super.open();
-  }
+		if (_canProceed) {
+			this.dispatchEvent(
+				new CustomEvent('accept-modal', { bubbles: true, composed: true })
+			);
+			this.close();
+		}
+	}
 
-  connection () {
-    return new Promise(
-      function (resolve, reject) {
-        this.open();
-        this._resolve = resolve;
-        this._reject = reject;
-      }.bind(this)
-    );
-  }
+	isValid() {
+		return true;
+	}
 
-  _gotoPreviousPage () {
-    this.dispatchEvent(new CustomEvent('reject-modal', { bubbles: true, composed: true }));
-    this.close();
-  }
+	_resetReason() {
+		if (this.options.needs_reason) {
+			this.needsReason = true;
+		} else {
+			this.needsReason = false;
+		}
 
-  _gotoNextPage () {
-    let _canProceed = true
+		this.reason = undefined;
+		if (this.shadowRoot.querySelector('paper-input')) {
+			this.shadowRoot.querySelector('paper-input').invalid = false;
+		}
+	}
 
-    if (this.options?.needs_reason) {
-      _canProceed = this.shadowRoot.querySelector('paper-input').validate()
-    }
-
-    if (_canProceed) {
-      this.dispatchEvent(new CustomEvent('accept-modal', { bubbles: true, composed: true }));
-      this.close();
-    }
-  }
-
-  isValid () {
-   return true;
-  }
-
-  _resetReason() {
-    if (this.options.needs_reason) {
-      this.needsReason = true
-    } else {
-      this.needsReason = false
-    }
-
-    this.reason = undefined
-    if (this.shadowRoot.querySelector('paper-input')){
-      this.shadowRoot.querySelector('paper-input').invalid = false
-    }
-  }
-
-  _onKeyDown (event) {
-    if ( event.keyCode == 13 ) {
-      this._gotoNextPage();
-    }
-  }
+	_onKeyDown(event) {
+		if (event.keyCode == 13) {
+			this._gotoNextPage();
+		}
+	}
 }
 
 window.customElements.define(ConfirmWizardConfirmModal.is, ConfirmWizardConfirmModal);
